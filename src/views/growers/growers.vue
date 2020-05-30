78b23d1d3b410a5d8ef8fd3d7d756818b161867b
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-05-30 17:01:24 +1200

    api filtered to display growers on grower page

--- src/views/growers/growers.vue
+++ src/views/growers/growers.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="text-left">
-    <h1>User information from API GET request</h1>
+    <h1>Grower information from API GET request</h1>
     <div v-for="(user, index) in users" :key="`fruit-${index}`">
-      <br />
-      <h2>User : {{ user.user_name }}</h2>
-      <P><b>First name</b></P>
-      <p>{{ user.first_name }}</p>
-      <P><b>Last name</b></P>
-      <p>{{ user.last_name }}</p>
-      <P><b>Phone number</b></P>
-      <p>{{ user.phone_number }}</p>
-      <P><b>Email address</b></P>
-      <p>{{ user.email_address }}</p>
-      <br />
+      <di v-if="user.got_apples_member === 1">
+        <br />
+        <h2>User : {{ user.user_name }}</h2>
+        <P><b>First name</b></P>
+        <p>{{ user.first_name }}</p>
+        <P><b>Last name</b></P>
+        <p>{{ user.last_name }}</p>
+        <P><b>Phone number</b></P>
+        <p>{{ user.phone_number }}</p>
+        <P><b>Email address</b></P>
+        <p>{{ user.email_address }}</p>
+        <br />
+      </di>
     </div>
   </div>
 </template>
